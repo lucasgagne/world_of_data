fd33136f078b4082056bcab8d52cbeaf2b964d55
lucasgagne <lucasgagne@berkeley.edu>
2025-08-25 08:40:57 -0700

    featured plot working on population page

--- plots_population.js
+++ plots_population.js
@@ -1,28 +1,27 @@
-// List of plot filenames (match the files you saved in the "plots/" folder)
-const plotFiles = [
-  "IN_population_2020.png",
-  "CN_population_2020.png",
-  "US_population_2020.png",
-  "ID_population_2020.png",
-  "PK_population_2020.png",
-  "NG_population_2020.png",
-  "BR_population_2020.png",
-  "BD_population_2020.png",
-  "RU_population_2020.png",
-  "ET_population_2020.png",
-  "MX_population_2020.png",
-  "JP_population_2020.png"
-];
 
-// Select container
-const container = document.getElementById('plots-container');
+  const plotFiles = [
+    "IN_population_2020.png",
+    "CN_population_2020.png",
+    "US_population_2020.png",
+    "ID_population_2020.png",
+    "PK_population_2020.png",
+    "NG_population_2020.png",
+    "BR_population_2020.png",
+    "BD_population_2020.png",
+    "RU_population_2020.png",
+    "ET_population_2020.png",
+    "MX_population_2020.png",
+    "JP_population_2020.png"
+  ];
 
-// Loop through plots and create <img> elements dynamically
-plotFiles.forEach(file => {
-  const img = document.createElement('img');
-  img.src = `plots/population/${file}`;
-  img.alt = file;  // accessibility
-  img.style.width = "300px";  // adjust size as needed
-  img.style.margin = "10px";  // spacing
-  container.appendChild(img);
-});
+  const container = document.getElementById('plots-container');
+
+  plotFiles.forEach(file => {
+    const img = document.createElement('img');
+    img.src = `plots/population/${file}`;
+    img.alt = file;
+    img.style.width = "300px";
+    img.style.margin = "10px";
+    container.appendChild(img);
+  });
+
