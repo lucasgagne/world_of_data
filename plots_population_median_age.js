122571f4b739e5b522870b84457246325a89b0c6
lucasgagne <lucasgagne@berkeley.edu>
2025-08-25 14:47:47 -0700

    population page is done

--- plots_population_median_age.js
+++ plots_population_median_age.js
@@ -1,21 +1,21 @@
-const populationPlotFiles = [
-    "BD_population.png",
-    "BR_population.png",
-    "CN_population.png",
-    "ET_population.png",
-    "ID_population.png",
-    "IN_population.png",
-    "JP_population.png",
-    "MX_population.png",
-    "NG_population.png",
-    "PK_population.png",
-    "RU_population.png",
-    "US_population.png",
+const medianPlotFiles = [
+    "BD_median_age.png",
+    "BR_median_age.png",
+    "CN_median_age.png",
+    "ET_median_age.png",
+    "ID_median_age.png",
+    "IN_median_age.png",
+    "JP_median_age.png",
+    "MX_median_age.png",
+    "NG_median_age.png",
+    "PK_median_age.png",
+    "RU_median_age.png",
+    "US_median_age.png",
     //"WD_population.png"
   ];
-  const containerMedian = document.getElementById('plots-container-growth');
+  const containerMedian = document.getElementById('plots-container-median-age');
 
-  populationPlotFiles.forEach(file => {
+medianPlotFiles.forEach(file => {
     const img = document.createElement('img');
     img.src = `plots/median_age/${file}`;
     img.alt = file;
