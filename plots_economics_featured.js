5dc3546011aee4967c1e2ab9109f8100aeb8d858
lucasgagne <lucasgagne@berkeley.edu>
2025-08-25 09:41:34 -0700

    featured graph section is working

--- plots_economics_featured.js
+++ plots_economics_featured.js
@@ -1,19 +1,19 @@
-// plots_population_featured.js
+// plots_economics_featured.js
 
-// List of population plot files
-const populationPlots = [
-  "IN_population_2020.png",
-  "CN_population_2020.png",
-  "US_population_2020.png",
-  "ID_population_2020.png",
-  "PK_population_2020.png",
-  "NG_population_2020.png",
-  "BR_population_2020.png",
-  "BD_population_2020.png",
-  "RU_population_2020.png",
-  "ET_population_2020.png",
-  "MX_population_2020.png",
-  "JP_population_2020.png"
+// List of economics plot files
+const economicsPlots = [
+    "USA_gdp.png",
+    "CHN_gdp.png",
+    "IND_gdp.png",
+    "JPN_gdp.png",
+    "DEU_gdp.png",
+    "GBR_gdp.png",
+    "FRA_gdp.png",
+    "ITA_gdp.png",
+    "BRA_gdp.png",
+    "CAN_gdp.png",
+    "RUS_gdp.png",
+    "MEX_gdp.png"
 ];
 
 // Get references to the select and image elements
@@ -21,17 +21,17 @@ const featuredSelect = document.getElementById("featured-select");
 const featuredImg = document.getElementById("featured-plot-img");
 
 // Populate dropdown dynamically
-populationPlots.forEach(file => {
+economicsPlots.forEach(file => {
   const option = document.createElement("option");
   option.value = file;
-  option.textContent = file.replace("_population_2020.png", "").replace("_", " "); // nicer label
+  option.textContent = file.replace("_economics_2020.png", "").replace("_", " "); // nicer label
   featuredSelect.appendChild(option);
 });
 
 // Set initial featured plot
-featuredImg.src = `plots/population/${populationPlots[0]}`;
+featuredImg.src = `plots/economics/${economicsPlots[0]}`;
 
 // Update featured plot when dropdown changes
 featuredSelect.addEventListener("change", (e) => {
-  featuredImg.src = `plots/population/${e.target.value}`;
+  featuredImg.src = `plots/economics/${e.target.value}`;
 });
